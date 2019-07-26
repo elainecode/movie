7b66c6c9eb35ae6b14c1bdb9d1b3fc71b4a0ed6e
Elaine Onuegbe <elaine_onuegbe@epam.com>
2019-07-26 13:58:20 -0700

    file setup

---
+++ README.md
@@ -0,0 +1,3 @@
+React Task 
+The idea is to create a single page application, which will allow users to search “The Movie DB” database. 
+There will be two pages: main page with search and a page with a particular film. 
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  watch: false, 
+  mode: 'development', 
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:3000",
+          secure: false,
+          changeOrigin: true
+        }
+      }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: ["react-hot-loader/webpack", "babel-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
