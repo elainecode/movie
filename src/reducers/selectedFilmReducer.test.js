1d11557b95cd317c42900aa1685619f1bc33e304
Elaine Onuegbe <elaine_onuegbe@epam.com>
2019-09-16 16:14:45 -0700

    9 tests in reducers

---
+++ src/reducers/selectedFilmReducer.test.js
@@ -0,0 +1,44 @@
+import selectedFilmReducer from './selectedFilmReducer';
+import {
+  SELECTED_FILM_SUCCESS,
+  RESET_SELECTED_TO_DEFAULT_STATE,
+} from '../actions/actionTypes';
+
+describe('INITIAL_STATE', () => {
+  test('returns initial state', () => {
+    const action = { type: 'dummy_action' };
+    const INITIAL_STATE = {};
+    expect(selectedFilmReducer(undefined, action)).toEqual(
+      INITIAL_STATE,
+    );
+  });
+});
+
+describe('SELECTED_FILM_SUCCESS', () => {
+  test('returns correct state when action type = SELECTED_FILM_SUCCESS', () => {
+    const action = {
+      type: SELECTED_FILM_SUCCESS,
+      film: { selected: 'selected film' },
+    };
+    const expectedState = {
+      selected: 'selected film',
+    };
+
+    expect(selectedFilmReducer(undefined, action)).toEqual(
+      expectedState,
+    );
+  });
+});
+
+describe('RESET_SELECTED_TO_DEFAULT_STATE', () => {
+  test('returns correct state when action type = RESET_SELECTED_TO_DEFAULT_STATE', () => {
+    const action = {
+      type: RESET_SELECTED_TO_DEFAULT_STATE,
+    };
+    const expectedState = {};
+
+    expect(selectedFilmReducer(undefined, action)).toEqual(
+      expectedState,
+    );
+  });
+});
---
+++ src/reducers/totalResultsReducer.test.js
@@ -0,0 +1,26 @@
+import totalResultsReducer from './totalResultsReducer';
+import { TOTAL_RESULTS } from '../actions/actionTypes';
+
+describe('INITIAL_STATE', () => {
+  test('returns initial state', () => {
+    const action = { type: 'dummy_action' };
+    const INITIAL_STATE = 0;
+    expect(totalResultsReducer(undefined, action)).toEqual(
+      INITIAL_STATE,
+    );
+  });
+});
+
+describe('TOTAL_RESULTS', () => {
+  test('returns correct state when action type = TOTAL_RESULTS', () => {
+    const action = {
+      type: TOTAL_RESULTS,
+      count: 55,
+    };
+    const expectedState = 55;
+
+    expect(totalResultsReducer(undefined, action)).toEqual(
+      expectedState,
+    );
+  });
+});
