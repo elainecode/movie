d87a2c6d89111ce8639147e6a962eee3222f0a56
Elaine Onuegbe <elaine_onuegbe@epam.com>
2019-10-02 12:00:07 -0700

    more small changes to tests

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -13,26 +13,3 @@ export const scrollToContentDivTop = (e, contentEl) => {
 
 export const findGenre = (array, genres) =>
   array.map(id => genres[id]);
-
-export const filmSorter = (sortBy, films) => {
-  switch (sortBy) {
-    case 'popularity.desc':
-      return films.sort((a, b) => b.popularity - a.popularity);
-    case 'popularity.asc':
-      return films.sort((a, b) => a.popularity - b.popularity);
-    case 'vote_count.desc':
-      return films.sort((a, b) => b.vote_count - a.vote_count);
-    case 'vote_count.asc':
-      return films.sort((a, b) => a.vote_count - b.vote_count);
-    case 'release_date.desc':
-      return films.sort(
-        (a, b) => new Date(b.release_date) - new Date(a.release_date),
-      );
-    case 'release_date.asc':
-      return films.sort(
-        (a, b) => new Date(a.release_date) - new Date(b.release_date),
-      );
-    default:
-      return films;
-  }
-};
--- src/helpers/index.test.js
+++ src/helpers/index.test.js
@@ -1,20 +1,22 @@
-import { scrollToContentDivTop } from './index';
+import { scrollToContentDivTop, findGenre } from './index';
 
 describe('scrollToContentDivTop', () => {
   test('function called on click event', () => {
     const mockScroll = jest.fn(scrollToContentDivTop);
+    const mockContentElFn = jest.fn(() => contentEl);
     const e = new Event('click', {
       bubbles: true,
       cancelable: true,
     });
     const contentEl = document.createElement('div');
     const bodyEl = document.createElement('div');
-    contentEl.className = 'MuiCardMedia';
+    contentEl.className = 'MuiCardMed;ia';
+    contentEl.scrollTo = jest.fn();
     bodyEl.append(contentEl);
     bodyEl.addEventListener(
       'click',
       event => {
-        mockScroll(event, () => contentEl);
+        mockScroll(event, mockContentElFn);
       },
       false,
     );
@@ -23,19 +25,18 @@ describe('scrollToContentDivTop', () => {
   });
 });
 
-// describe('test for scrollToContentDivTop', () => {
-//   test('', () => {
-//     const e = new Event('click', {
-//       bubbles: false,
-//       cancelable: true,
-//     });
-//     const contentEl = document.createElement('div');
-//     const bodyEl = document.createElement('div');
-//     contentEl.className = 'MuiCardMedia';
-//     bodyEl.append(contentEl);
-//     beforeAll(() => {
-//       contentEl.dispatchEvent(e);
-//     });
-//     expect(scrollToContentDivTop(e, contentEl)).toHaveBeenCalled();
-//   });
-// });
+describe('test for findGenre', () => {
+  test('function returns correct values', () => {
+    const mockFindGenre = jest.fn(findGenre);
+    const genres_from_film = [14, 28, 36];
+    const genres = {
+      28: 'Action',
+      12: 'Adventure',
+      14: 'Fantasy',
+      36: 'History',
+    };
+    const mapped = ['Fantasy', 'Action', 'History'];
+
+    expect(mockFindGenre(genres_from_film, genres)).toEqual(mapped);
+  });
+});
